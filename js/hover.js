06f444898e0559b9fa212c7df2d15ec841c6107e
이동수 <rktks204@sungkyul.ac.kr>
2023-04-12 05:16:05 +0000

    자바웹프로그래밍 6주차 실습

---
+++ js/hover.js
@@ -0,0 +1,7 @@
+function over(obj){
+	obj.src = "img/Google_logo_1998.png";
+}
+
+function out(obj){
+	obj.src = "img/Google_logo_color.png";
+}
--- js/search.js
+++ js/search.js
@@ -1,8 +1,21 @@
 document.getElementById("search_btn").addEventListener('click', search_message);
 
 function search_message(){
-	// alert("검색을 수행합니다!");
+	
 	let search_str = document.querySelector("#search_txt"); //변수에 저장
-	document.getElementById("search_message").innerHTML = search_str.value; //태그에 값 추가
-	console.log(search_str.value);
+	
+	if(search_str.value.length === 0) // 문자 길이, 엄격 비교
+	{
+		alert("검색어가 비었습니다. 입력해주세요.")
+	}
+	
+	else if(search_str.value === "123" || search_str.value === "456" || search_str.value === "789"){
+		alert("비속어는 검색할 수 없습니다.")
+	}
+	else{
+		alert("검색을 수행합니다!");
+		let text = document.getElementById("search_message").innerHTML = search_str.value; //태그에 값 추가
+		//console.log(search_str.value);
+		document.querySelector("#form_main").submit();
+	}
 }
